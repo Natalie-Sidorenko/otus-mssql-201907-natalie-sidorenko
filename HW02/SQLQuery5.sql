7f54d9c0536dc57f5e35fd866af4cb698a6e1b38
Natalie Sidorenko <noethertheorem@yandex.ru>
2019-08-19 18:02:16 +0300

    update SQLQuery5.sql

--- HW02/SQLQuery5.sql
+++ HW02/SQLQuery5.sql
@@ -3,6 +3,6 @@ SELECT TOP (10) o.[OrderID]
       ,c.[CustomerName]
       ,p.[FullName] AS Salesperson
       ,o.[OrderDate]
-  FROM [WideWorldImporters].[Sales].[Orders] o JOIN [WideWorldImporters].[Sales].[Customers] c ON o.[CustomerID]=o.[CustomerID]
+  FROM [WideWorldImporters].[Sales].[Customers] c JOIN [WideWorldImporters].[Sales].[Orders] o ON o.[CustomerID]=o.[CustomerID]
   JOIN [WideWorldImporters].[Application].[People] p ON o.[SalespersonPersonID]=p.[PersonID]
   ORDER BY o.[OrderDate] DESC
